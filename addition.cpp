f43df33382ff7914f0b7afa9f463d879067e643e
sriranjanijp <sriranjani.jp@gmail.com>
2025-03-12 22:30:13 +0530

    Engeyum Eppothum - GV Prakash Kumar

---
+++ addition.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int var1,var2;
+    var1 = 10;
+    var2 = 20;
+    cout << "var1 + var2 is this\n";
+    cout << var2+var1 << endl;
+    cin.get();
+    return 0;
+}
